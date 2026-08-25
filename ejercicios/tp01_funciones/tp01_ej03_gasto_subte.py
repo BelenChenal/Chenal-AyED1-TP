baa979956b8d371dd7902625f263d9607e1a4bea
BelenChenal <ayuzawa26@gmail.com>
2026-08-25 19:48:42 -0300

    tp01_ej3_sinterminarpt2

--- ejercicios/tp01_funciones/tp01_ej03_gasto_subte.py
+++ ejercicios/tp01_funciones/tp01_ej03_gasto_subte.py
@@ -1,23 +1,47 @@
-def calculo_descuento (base,cantidad,descuento):
+def calculo_descuento (base:float,cantidad:int,descuento:float)-> float:
+    """
+        Pre: 
+        Post: 
+    
+    """
     resultado = (base * cantidad) * descuento
 
     return resultado
 
-def calculo_gasto_subte(cant_viajes):
+def calculo_gasto_subte(cant_viajes:int)->float:
+    """
+    
+        Pre: 
+        Post: 
+    
+    """
     cantidad = cant_viajes 
     base_boleto = 1500
     descuento1= 0.80
     descuento2= 0.70
-    descuento3= 0.60
-    total= 0 
+    descuento3= 0.60 
+    v1 = 0
+    v2 = 0
+    v3 = 0
+    v4 = 0
+    total= 0
     if cant_viajes >= 1 and cant_viajes <= 20: # no tiene descuento, paga el total
         total= base_boleto * cant_viajes
-    elif cant_viajes >= 21 and cant_viajes <=30: # 20% de descuento entre 21 y 30
-        total= calculo_descuento(base_boleto,cantidad,descuento1)
-    elif cant_viajes >= 31 and cant_viajes <= 40:# 30% de descuento entre 31 y 40
-        total= calculo_descuento(base_boleto,cantidad,descuento2)
-    elif cant_viajes > 40: # 40% de descuento, mas del 40
-        total= calculo_descuento(base_boleto,cantidad,descuento3)
+    elif cant_viajes >= 21 and cant_viajes <=30: # 20% de descuento entre 21 y 30 viajes
+        v1 = 20 * base_boleto
+        v2= calculo_descuento(base_boleto,cantidad-20,descuento1)
+        total= v1 + v2
+    elif cant_viajes >= 31 and cant_viajes <= 40:# 30% de descuento entre 31 y 40 viajes
+        v1= 20 * base_boleto
+        v2= calculo_descuento(base_boleto,10,descuento1)
+        v3= calculo_descuento(base_boleto,cantidad -30 , descuento2)
+        total=  v1 + v2 + v3 
+    elif cant_viajes > 40: # 40% de descuento, mas de 40 viajes
+        v1= 20 * base_boleto
+        v2= calculo_descuento(base_boleto,10,descuento1)
+        v3= calculo_descuento(base_boleto,10,descuento2)
+        v4= calculo_descuento(base_boleto,cantidad -40,descuento3)
+        total= v1 +  v2 + v3 + v4
 
     return total 
 
