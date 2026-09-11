143692cf357e7439b8e933690fac7b76fa059300
BelenChenal <ayuzawa26@gmail.com>
2026-09-11 10:53:58 -0300

    06

--- ejercicios/tp01_funciones/tp01_ej06_concatenar_enteros.py
+++ ejercicios/tp01_funciones/tp01_ej06_concatenar_enteros.py
@@ -3,13 +3,15 @@
 #parámetros. Por ejemplo, si recibe 1234 y 567 debe devolver 1234567. No se per-
 #mite utilizar facilidades de Python no vistas en clase
 
-def concatenar (a:str, b:str) ->str: 
-    lista= [a,b]
-    unir = "".join(lista)
+def concatenar (a:int, b:int) ->int: 
+    contador = 0
+    aux= b
+    while aux != 0:
+        aux //= 10
+        contador += 1
+    unir= a *(10**contador) + b 
 
-    return unir
 
-
-num1= (input('Ingrese primer numero'))
-num2= (input('Ingrese segundo numero'))
+num1= (input('Ingrese primer numero '))
+num2= (input('Ingrese segundo numero '))
 print(concatenar(num1,num2))
